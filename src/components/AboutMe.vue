18bdf544937c9edc2764920bcf255ece98f969e2
Igor Lira <igorlirap@gmail.com>
2022-10-01 00:44:16 -0300

    add about me component

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+<template>
+  <div class="content-container">
+    <div class="content-1"> ABOUT ME </div>
+    <div class="content-2"> WHO AM I </div>
+    <div class="content-3">
+      I double graduated in Software Engineering at Centrale Nantes (France) and Civil Engineering
+      at the Federal University of Ceará (Brazil). I have over two years of experience in web development
+      in full-stack projects.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.content-container {
+  margin-top: 40px;
+  margin-left: 10%;
+}
+
+.content-1 {
+  font-family: 'Itim';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 43px;
+  color: #000000;
+}
+
+.content-2 {
+  font-family: 'Itim';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 80px;
+  line-height: 96px;
+  color: #C4C4C4;
+}
+
+.content-3 {
+  width: 80%;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+  color: #444444;
+}
+</style>
