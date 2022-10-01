75102a714bac7aedb4086637b768ad787f2d3439
Igor Lira <igorlirap@gmail.com>
2022-10-01 00:43:21 -0300

    add presentation card component

---
+++ src/components/PresentationCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+
+</script>
+  
+<template>
+  <div class="presentation-card">
+    <div class="presentation-content">
+      <div class="presentation-content-1">Hello, I'm</div>
+      <div class="presentation-content-2">Igor</div>
+      <div class="presentation-content-1">Web Developer</div>
+      <div class="buttons-container">
+        <button> Resume </button>
+        <button> Github</button>
+        <button> LinkedIn </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.presentation-card {
+  position: relative;
+  left: 10%;
+  height: 426px;
+  width: 85%;
+  background: red;
+  background: #FDFDFD;
+  box-shadow: 3px 4px 4px rgba(163, 159, 159, 0.25)
+}
+
+.presentation-content {
+  position: absolute;
+  left: 10%;
+  top: 30%;
+  text-align: left;
+  width: 355px;
+  height: 130px;
+}
+
+.presentation-content-1 {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 100;
+  font-size: 36px;
+  line-height: 42px;
+  color: #000000;
+}
+
+.presentation-content-2 {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 47px;
+  color: #000000;
+}
+
+.buttons-container>button {
+  width: 80px;
+  margin-left: 10px;
+  background: #3A3737;
+  border-radius: 10px;
+  color: #FFFFFF;
+  font-family: 'Itim';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+}
+</style>
